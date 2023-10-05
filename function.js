2310cb5b6a00d90cabc6deb6337f8e2b314927b1
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-06 00:22:38 +0600

    ok

---
+++ function.js
@@ -0,0 +1,8 @@
+// function declareation
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+// call the function
+startFan();
