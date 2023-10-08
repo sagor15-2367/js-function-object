dc338caed2545644ea8d0c69426c4a47efd7931f
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-08 14:37:28 +0600

    ok

---
+++ objects.js
@@ -0,0 +1,23 @@
+var student={
+    id:115,
+    name:'sagor alam',
+    class:9,
+    mark:98
+}
+
+var mobile={
+    brand:'samsung',
+    price:19000,
+    storage:'64gb',
+    camera:'10HP'
+}
+
+var myComputer={
+    brand:'lonovo',
+    price:1000000,
+    color:'silver',
+    processor:'i5'
+}
+console.log(myComputer.color);
+myComputer.color='white';
+console.log(myComputer);
