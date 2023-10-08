6083e19dee1822a15ce038159f9ff0fda67739f2
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-08 15:34:13 +0600

    ok

---
+++ switch.js
@@ -0,0 +1,41 @@
+const color='';
+if(color==='green'){
+    console.log('you are a green friends');
+}
+else if(color==='blue'){
+    console.log('you are a blue friends');
+}
+else if(color==='red'){
+    console.log('you are a red friends');
+}
+else if(color==='white'){
+    console.log('you are a white friends');
+}
+else if(color==='yellow'){
+    console.log('you are a Yellow friends');
+}
+else{
+    console.log('you are a black friends');
+}
+
+//switch
+
+switch(color){
+    case 'green':
+        console.log('you are a green friends');
+        break;
+    case 'blue':
+        console.log('you are a blue friends');
+        break;
+    case 'white':
+        console.log('you are a white friends');
+        break;
+    case 'red':
+        console.log('you are a red friends');
+        break;
+    case 'yellow':
+        console.log('you are a Yellow friends');
+        break;
+    default:
+        console.log('you are a no friends');
+}
